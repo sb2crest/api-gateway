e1c29d3ae0cb73e74ac84ec9a6f5131818b2d43c
Javed006 <shaik.jvd006@gmail.com>
2024-02-22 12:42:16 +0530

    password Validation

--- src/main/java/com/fdapn/service/UserService.java
+++ src/main/java/com/fdapn/service/UserService.java
@@ -1,6 +1,8 @@
 package com.fdapn.service;
 
+import com.fdapn.auth.AuthenticationService;
 import com.fdapn.dao.UserRepository;
+import com.fdapn.exception.InvalidPasswordException;
 import com.fdapn.model.ChangePasswordRequest;
 import com.fdapn.model.User;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +18,19 @@ import java.security.Principal;
 public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository repository;
+    private final AuthenticationService authenticationService;
+
     public void changePassword(ChangePasswordRequest request, Principal connectedUser) {
-        var user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+        User user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+        boolean isPasswordValidate = authenticationService.passwordValid(request.getNewPassword());
         if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
-            throw new IllegalStateException("Wrong password");
+            throw new InvalidPasswordException("Wrong password");
+        }
+        if (!isPasswordValidate) {
+            throw new InvalidPasswordException("Please enter valid password.");
         }
         if (!request.getNewPassword().equals(request.getConfirmationPassword())) {
-            throw new IllegalStateException("Passwords do not match");
+            throw new InvalidPasswordException("Passwords do not match");
         }
         user.setPassword(passwordEncoder.encode(request.getNewPassword()));
         repository.save(user);
