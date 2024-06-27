15742c1c201f66c034e911b2d2efb385eb34b123
prajwal <prajwal@seabed2crest.com>
2024-06-27 12:47:21 +0530

    fixed the bug in user validation in registration

--- src/main/java/com/fdapn/dao/UserRepository.java
+++ src/main/java/com/fdapn/dao/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
   @Query("SELECT u FROM User u WHERE u.email = :email")
   Optional<User> findByEmail(@Param("email") String email);
